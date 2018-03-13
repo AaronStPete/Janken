0020730b55f9eab9b5ebe90b71886d4ee01a0994
Wasserman <awasserman@proforma.com>
2018-03-13 14:00:26 -0400

    js logs player choice

--- public/main.js
+++ public/main.js
@@ -1,5 +1,54 @@
-const main = () => {
-  document.querySelector('h1').textContent += '?'
-}
+// const main = () => {
+//   document.querySelector('h1').textContent += '?'
+// }
 
-document.addEventListener('DOMContentLoaded', main)
+// document.addEventListener('DOMContentLoaded', main)
+
+
+// Player 1 makes a choice
+let choicePlayer1 = "none";
+  // button is clicked and choicePlayer1 is stored
+let p1Rock = () => {
+  let choicePlayer1 = "rock"
+  console.log (choicePlayer1);
+}
+let p1Paper = () => {
+  let choicePlayer1 = "paper"
+  console.log (choicePlayer1);
+}
+let p1Scissors = () => {
+  let choicePlayer1 = "scissors"
+  console.log (choicePlayer1);
+}
+console.log (choicePlayer1);
+  // player1Window is hidden
+// Player 2 makes a choice
+let choicePlayer2 = "none";
+  // button is clicked and choicePlayer2 is stored
+let p2Rock = () => {
+  let choicePlayer2 = "rock"
+  console.log (choicePlayer2);
+}
+let p2Paper = () => {
+  let choicePlayer2 = "paper"
+  console.log (choicePlayer2);
+}
+let p2Scissors = () => {
+  let choicePlayer2 = "scissors"
+  console.log (choicePlayer2);
+}
+console.log (choicePlayer2);
+  // player2Window is hidden
+// the rules are defined
+  // paper beats rock
+  // rock beats scissors
+  // scissors beats paper
+  // there is a tie
+// the choices are compared
+  // winner is decided
+// an outcome is displayed
+  // show whoWon
+  // log winner with choicePlayer1 & choicePlayer2
+  // offer reset button
+// reset button
+  // 
